bcd48b7190656bcd2de9ac8e343eb88ba514b2bb
BerlianoSurya <berlianosurya@gmail.com>
2025-02-19 16:34:04 +0700

    fix some error

--- app/dashboard/events/page.tsx
+++ app/dashboard/events/page.tsx
@@ -1,7 +1,5 @@
 import { getAllEvents } from '@/utils/events'
 import { getCurrentUser } from '@/utils/users'
-import Link from 'next/link'
-import EventList from '@/components/EventList'
 import EventsTable from '@/components/EventsTable'
 
 const Events = async () => {
@@ -13,7 +11,6 @@ const Events = async () => {
       <div className="w-full justify-center my-6 text-4xl">
         <h1 className="text-center font-extrabold">Event List</h1>
       </div>
-      {/* <EventList data={events} /> */}
       <EventsTable events={events} />
     </div>
   )
